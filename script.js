189fe4381502c93a09d91fbc2ba052be88d8b0c8
Bojan Mitrusic <b.mitrusic2000@gmail.com>
2024-09-24 23:45:06 +0200

    URL is written properly

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const jokeBtn = document.getElementById("joke-btn");
 
 function generateJoke() {
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.chucknorris.io/jokes/random6");
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
   xhr.onreadystatechange = function () {
     if (this.readyState === 4) {
       if (this.status === 200) {
